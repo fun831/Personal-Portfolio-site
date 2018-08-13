615eb813cdde609bfca3d41a6bd5b110d733e46e
Matt Farnsworth <mryanfarnsworth@gmail.com>
2018-08-13 11:59:29 -0600

    Added some basic content in contact page.

--- src/components/app.js
+++ src/components/app.js
@@ -10,8 +10,8 @@ export default class App extends Component {
         <NavBar/>
         <div className='home-page-content-container'>
           <div className="content-container">
-          <div className='pic'>
-            <img src="https://i.kym-cdn.com/entries/icons/facebook/000/018/385/Rs_634x1024-130605092844-634.DespMe2.mh.060513.jpg" alt="profile pic or something similar"/>
+            <div className='pic'>
+              <img src="https://i.kym-cdn.com/entries/icons/facebook/000/018/385/Rs_634x1024-130605092844-634.DespMe2.mh.060513.jpg" alt="profile pic or something similar"/>
             </div>
             <p>
             Hello and welcome to my portfolio site. At the top you can navigate between pages
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import Navbar from './navBar';
 
 class Contact extends Component {
@@ -7,6 +9,21 @@ class Contact extends Component {
             <div className="contact">
                 <Navbar/>
                 <h1>Contact Page</h1>
+                <div className='home-page-content-container'>
+                <div className="content-container">
+                    <div className='pic'>
+                        <img src="https://i.kym-cdn.com/entries/icons/facebook/000/018/385/Rs_634x1024-130605092844-634.DespMe2.mh.060513.jpg" alt="profile pic or something similar"/>
+                    </div>
+                    <a href="https://www.linkedin.com/in/matthew-farnsworth/" target="blank" className="link">LinkedIn</a>
+                    <a href="https://www.github.com/fun831/" target="blank" className='link'>GitHub</a>
+                    <a href="https://drive.google.com/open?id=1JrMJJkZ9hyIswig9PBB4PemAYZvBALw5" target="blank" className='link'>My Resume</a>
+                    <p>
+                    Hello and welcome to my portfolio site. At the top you can navigate between pages
+                    to learn more about me, my education, and projects I have done. 
+                    </p>
+                    <Link to="/projects" className="projects-button-link">See my projects</Link>
+                </div>
+                </div>
             </div>
         );
     }
