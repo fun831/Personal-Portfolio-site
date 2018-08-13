1313fa9f05dcc772f710d90fe66a9db7753c8c81
Matt Farnsworth <mryanfarnsworth@gmail.com>
2018-08-13 11:42:19 -0600

    Added some filler content for the about me page.

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -7,6 +7,19 @@ class AboutMe extends Component {
             <div className="about_me">
                 <Navbar/>
                 <h1>About Me Page</h1>
+                <p>
+                I'm Matt Farnsworth, 
+                I’m a developer who enjoys solving problems through programming. 
+                I take pride in my work and strive to make my projects clean, 
+                efficient, and built to last. I enjoy learning new things and 
+                expanding my knowledge. Working as a finish carpenter I have 
+                developed skills to be attentive to detail. I have the ablility 
+                to dig into a project until the job is done and be able to find 
+                solutions to complex problems. It is rewarding finding a solution 
+                to a challenge I’m faced with. Besides programming some things 
+                I enjoy are the outdoors, being with family, and designing and 
+                building wood projects.
+                </p>
             </div>
         );
     }
